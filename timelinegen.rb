f0bd44689ba33b66be7b66e543ad72c0c65e4f54
Shidash <shidash@shidash.com>
2013-05-15 03:28:19 -0400

    Added date parsing

--- timelinegen.rb
+++ timelinegen.rb
@@ -2,30 +2,29 @@ require 'json'
 
 class TimelineGen
 
-  # Get start date
-  def self.startDate(date)
-    date
+  # Change date format to work with TimelineJS
+  def self.parseDate(date)
+    if date != nil
+      date.gsub!("-",",")
+      date.gsub!(" ",",")
+      date.gsub!(":",",")
+    end
   end
   
-  #Get end date
-  def self.endDate(date)
-    date
-  end
-  
-  #Generate JSON for event
-  def self.genEvent(sdate, edate, headline, text)
+  # Generate JSON for event
+  def self.genEvent(startDate, endDate, headline, text)
     JSON.pretty_generate(
-                         "startDate" => startDate(sdate),
-                         "endDate" => endDate(edate),
+                         "startDate" => parseDate(startDate),
+                         "endDate" => parseDate(endDate),
                          "headline" => headline,
                          "text" => text
                         )
   end
 
 
-  #Generate JSON for timeline
+  # Generate JSON for timeline
   def self.genTimeline
-    firstEvent = JSON.parse genEvent("10,10,10", nil, "test headline", "This is the text body of the email")
+    firstEvent = JSON.parse genEvent("2010-01-12 18:53:08", nil, "test headline", "This is the text body of the email")
     JSON.pretty_generate(
                          "timeline" => {
                            "headline" => "timeline name",
