f3fb5c5730c7cfef038467430e0673cfce72fc59
Shidash <shidash@shidash.com>
2013-12-17 16:18:05 -0800

    Timeline generator works for all JSONs, not just emails.

--- lib/timelinegen.rb
+++ lib/timelinegen.rb
@@ -2,18 +2,29 @@ require 'json'
 
 class TimelineGen
 
+  def initialize(input, headlinefield, textfield, startdatefield, enddatefield, timelinetitle)
+    @input = input
+    @textfield = textfield
+    @headlinefield = headlinefield
+    @startdatefield = startdatefield
+    @enddatefiled = enddatefield
+    @timelinetitle = timelinetitle
+  end
+
   # Change date format to work with TimelineJS
-  def self.parseDate(date)
+  def parseDate(date)
     if date != nil
       date.gsub!("-",",")
       date.gsub!(" ",",")
+      date.gsub!("T", ",")
+      date.gsub!("Z", "")
       date.gsub!(":",",")
     end
     date
   end
   
   # Generate JSON for event
-  def self.genEvent(startDate, endDate, headline, text)
+  def genEvent(startDate, endDate, headline, text)
     JSON.pretty_generate(
                          "startDate" => parseDate(startDate),
                          "endDate" => parseDate(endDate),
@@ -22,32 +33,60 @@ class TimelineGen
                         )
   end
 
-
-  # Generates all event JSONs
-  def self.parseEvents(file)
-    pe = JSON.parse(File.read(file))
+  # Parses events for timeline
+  def parseEvents
+    pe = JSON.parse(@input)
     k = pe.length
     event = Array.new
     (0..k-2).each do |i|
-        event[i] = JSON.parse(
+              event[i] = JSON.parse(
                               genEvent(
-                                       (pe[i+1])["date"], 
-                                       nil, 
-                                       (pe[i+1])["subject"], 
-                                       (pe[i+1])["body"]
+                                       checkSplit(@startdatefield, i, pe),
+                                       checkSplit(@enddatefield, i, pe),
+                                       checkSplit(@headlinefield, i, pe),
+                                       checkSplit(@textfield, i, pe)
                                        )
                               )
     end
     return event
   end
+
+  # Check to see if events should be split on comma
+  def checkSplit(field, num, pe)
+    if field != nil && field.include?(",")
+      return split(field, num, pe)
+    else 
+      return (pe[num+1])[field]
+    end
+  end
+
+  # Splits fields on comma and returns values
+  def split(field, num, pe)
+    fieldarray = field.split( /, */ )
+    resultstring = ""
+
+    fieldarray.each do |f|
+      resultstring = resultstring + f + ": " + checkNil(f, num, pe) + "\n"
+    end
+
+    return resultstring
+  end
+
+  # Check to see if value exists
+  def checkNil(f, num, pe)
+    if (pe[num+1])[f]
+      return (pe[num+1])[f].to_s
+    else return ""
+    end
+  end
   
-  #Gets the headline
-  def self.parseHeadline(file)
-    headline = (JSON.parse(File.read(file))[0])["emailheadline"]
+  # Gets the headline
+  def parseHeadline
+    headline = @timelinetitle
   end
 
   # Generates full timeline JSON
-  def self.genTimeline(event, headline)
+  def genTimeline(event, headline)
     JSON.pretty_generate(
                          "timeline" => {
                            "headline" => headline,
@@ -58,10 +97,9 @@ class TimelineGen
                        )
   end
 
-  def self.emailTimeline(file)
-    event = parseEvents(file)
-    headline = parseHeadline(file)
+  def timeline
+    event = parseEvents
+    headline = parseHeadline
     genTimeline(event, headline)
   end
 end
-
